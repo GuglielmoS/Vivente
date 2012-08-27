5a7e55fd60446414c1a80eb58f3fe609c5949683
GuglielmoS <guglielmos@inventati.org>
2012-08-27 23:27:11 +0200

    added comments

--- clife.lisp
+++ clife.lisp
@@ -1,5 +1,5 @@
 ;; Author: Guglielmo Fachini a.k.a. GuglielmoS
-;; Date:   08 August 2012
+;; Starting date: 08 August 2012
 ;; 
 ;; Implementation of the Conway's Game of Life
 ;;
@@ -10,6 +10,7 @@
 ;; If you want to change the size of world or the time interval between
 ;; a generation and another one, you can modify the parameter below.
 ;;
+;; Copyleft by GuglielmoS
 
 (defparameter *default-world-size* '(20 70))
 (defparameter *update-interval* 0.1)
@@ -17,6 +18,9 @@
 (defparameter *dead-cell* #\space)
 
 (defun gen-random-world (&optional (world-size *default-world-size*))
+  "Generates a random world with the preferred size. 
+   The data structure returned is a bidimensional array and the default size
+   is taken from the global variable *default-world-size*."
   (let ((world (make-array world-size :element-type 'bit)))
     (destructuring-bind (dim-x dim-y) world-size
       (loop for i from 0 below dim-x do
@@ -25,6 +29,7 @@
     world))
 
 (defun alive-neighbors (world pos)
+  "Returns the number of neighbors of the cell located at 'pos'."
   (destructuring-bind (dim-x dim-y x y) (append (array-dimensions world) pos)
     (let ((to-check (list 
 		    (list (mod (1- x) dim-x) (mod (1- y) dim-y))
@@ -35,31 +40,28 @@
 		     (list (mod (1+ x) dim-x) (mod (1- y) dim-y))
 		     (list (mod (1+ x) dim-x) y)
 		     (list (mod (1+ x) dim-x) (mod (1+ y) dim-y)))))
-      (length (remove-if
-	       (lambda (pos)
-		 (if (eql (aref world (car pos) (cadr pos)) 0)
-		     T
-		     nil))
+      (length 
+       (remove-if 
+	       (lambda (pos) 
+		 (if (eql (aref world (car pos) (cadr pos)) 0) T nil))
 	       to-check)))))
 
 (defun next-generation (world)
-  (let* ((dim (array-dimensions world))
-	(dim-x (car dim))
-	(dim-y (cadr dim))
-	(nw (make-array dim)))
-    (loop for i from 0 below dim-x do 
-	 (loop for j from 0 below dim-y do
-	      (let ((cur-state (aref world i j))
-		    (an (alive-neighbors world (list i j))))
-		(if (eq cur-state 0)
-		    (setf (aref nw i j) (if (eq an 3) 1 0))
-		    (setf (aref nw i j) (if (or (eq an 2) (eq an 3)) 1 0))))))
-    nw))
+  "Returns the next generation of the world passed as argoument."
+  (destructuring-bind (dim-x dim-y) (array-dimensions world)
+    (let ((nw (make-array (list dim-x dim-y))))
+      (loop for i from 0 below dim-x do
+	   (loop for j from 0 below dim-y do
+		(let ((cur-state (aref world i j))
+		      (an (alive-neighbors world (list i j))))
+		  (if (eq cur-state 0)
+		      (setf (aref nw i j) (if (eq an 3) 1 0))
+		      (setf (aref nw i j) (if (or (eq an 2) (eq an 3)) 1 0))))))
+      nw)))
 
 (defun print-world (world)
-  (let* ((dim (array-dimensions world))
-	(dim-x (car dim))
-	(dim-y (cadr dim)))
+  "Prints the world to the terminal."
+  (destructuring-bind (dim-x dim-y) (array-dimensions world)
     (loop for i from 0 below dim-x do
 	 (fresh-line)
 	 (loop for j from 0 below dim-y do
@@ -70,6 +72,7 @@
 
 (let ((generation 0))
   (defun life-loop (world)
+    "Shows the current world and jump to the next generation."
     (ext:run-program "clear")
     (format t "Generation: ~a" generation)
     (print-world world)
@@ -78,5 +81,6 @@
     (life-loop (next-generation world))))
 
 (defun evolve ()
+  "Starts the evolution of a random world."
   (life-loop (gen-random-world)))
 
